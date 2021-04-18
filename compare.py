9535612ab6c4f03875fd7303b2bebcc0894b0c50
MingRenTang123 <82753533+MingRenTang123@users.noreply.github.com>
2021-04-18 20:08:53 +0800

    Update and rename comparevstcrgp.py to compare.py

--- compare.py
+++ compare.py
@@ -15,6 +15,7 @@ for file in file_dash_human:
             print(test_size)
             print(Counter(y_train))
             print(Counter(y_test))
+            # ===========SemiTCR======================
             TT_list = []
             for i in range(5):
                 TT.fit(X_train, y_train, X_test)
@@ -22,14 +23,25 @@ for file in file_dash_human:
                 TT_list.append(acc_U)
             TT_mean = np.mean(TT_list)
             empty_TT.append(TT_mean)
+            # ===========SemiTCR-plus==================
+            DT = TT.classifiers[0]
+            SVC = TT.classifiers[1]
+            RF = TT.classifiers[2]
+            sclf_six = StackingClassifier(classifiers=[DT, SVC, RF, gbc, xgbc, MLP], meta_classifier=lr)
+            sclf_six.fit(X_train, y_train)
+            acc_six = sclf_six.score(X_test, y_test)
+            accs_six.append(acc_six)
+            # ============TCRGP=================
             acc_gp = tcrgp(data, train_idx, test_idx, filename)
             empty_GP.append(acc_gp)
             print(train_idx[:10])
-            print("TT:", TT_mean)
+            print("SemiTCR:", TT_mean)
+            print("SemiTCR-PLUS:", acc_six)
             print("TCRGP", acc_gp)
             print("========================")
     print(filename)
     print(test_size)
     print(np.mean(empty_TT))
+    print(np.mean(accs_six))
     print(np.mean(empty_GP))
     print("========================")
